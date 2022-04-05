957e7c6282c838d0a6a41862d37ad64147137e6a
Didierlan <98673729+Didierlan@users.noreply.github.com>
2022-04-05 10:29:28 -0500

    Newdev (#2)
    * Esta es una suma en js
    
    * calculadora basica vamos por mas

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+var a = 2; 
+var b = 4; 
+var R = 0; 
+
+//suma
+R = a + b; 
+console.log('suma ' + R); 
+
+//resta 
+R = a - b; 
+console.log('resta ' + R); 
+
+//divicion
+R = a / b; 
+console.log('divicion ' + R); 
+
+//multiplicacion
+R = a * b; 
+console.log('multiplicacion ' + R); 
