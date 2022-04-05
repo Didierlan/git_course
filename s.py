636f1fa31ba94c6a657dbbd2ab591bb84a7026f6
Didier Niño <dsnino561@gmail.com>
2022-04-05 10:09:30 -0500

    Calculadora basica en .py

---
+++ s.py
@@ -0,0 +1,18 @@
+a = int(input("ingrese un numero: "))
+b =  int(input("ingrese un numero: ")) 
+
+#suma 
+c = a + b 
+print("suma: " , c)
+
+#resta 
+c = a - b 
+print("resta: " , c)
+
+#divicion 
+c = a / b 
+print("divicion: " , c)
+
+#multipliacacion
+c = a * b 
+print("multiplicacion: " , c)
